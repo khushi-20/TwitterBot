36001cdc5458b9868c8974de8b44470e3403eaf8
Khushi Sharma <khushirke963@gmail.com>
2020-12-13 04:22:51 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -8,37 +8,31 @@ api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
 FILE_NAME = 'last_seen_id.txt'
 
-def retrieve_last_seen_id(file_name):
-    f_read = open(file_name, 'r')
+def retrieve_last_seen_id(FILE_NAME):
+    f_read = open(FILE_NAME, 'r')
     last_seen_id = int(f_read.read().strip())
     f_read.close()
     return last_seen_id
 
-def store_last_seen_id(last_seen_id, file_name):
-    f_write = open(file_name, 'w')
+def store_last_seen_id(FILE_NAME,last_seen_id):
+    f_write = open(FILE_NAME, 'w')
     f_write.write(str(last_seen_id))
     f_write.close()
     return
 
 def reply_to_tweets():
-    print('retrieving and replying to tweets...', flush=True)
-    # DEV NOTE: use 1060651988453654528 for testing.
-    last_seen_id = retrieve_last_seen_id(FILE_NAME)
     # NOTE: We need to use tweet_mode='extended' below to show
     # all full tweets (with full_text). Without it, long tweets
     # would be cut off.
-    mentions = api.mentions_timeline(
-                        last_seen_id,
-                        tweet_mode='extended')
-    for mention in reversed(mentions):
-        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
-        last_seen_id = mention.id
-        store_last_seen_id(last_seen_id, FILE_NAME)
-        if '#HelloWorldq' in mention.full_text.lower():
-            print('found #helloworld!', flush=True)
-            print('responding back...', flush=True)
-            api.update_status('@' + mention.user.screen_name +
-                    '#HelloWorld back to you!', mention.id)
+    tweets = api.mentions_timeline(retrieve_last_seen_id(FILE_NAME),tweet_mode='extended')
+    for tweet in reversed(tweets):
+       if '#HelloWorld123' in tweet.full_text.lower():
+            print(str(tweet.id) + ' - ' + tweet.full_text)
+            api.update_status('@' + tweet.user.screen_name +'#HelloWorld back to you!', tweet.id)
+            api.create_favorite(tweet.id)
+            api.retweet(tweet.id)
+            store_last_seen_id(FILE_NAME,tweet.id)
+   
 
 while True:
     reply_to_tweets()
